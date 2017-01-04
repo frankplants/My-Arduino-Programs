b980073d136d9e247b1c400b5e88f07fec1dd60a
Frank <fparsons143@gmail.com>
2017-01-03 19:02:24 -0500

    Update ScareBear.ino

--- ScareBear.ino
+++ ScareBear.ino
@@ -5,7 +5,7 @@ int RELAY1 = 1;
 //The pin that the relay is connected to.
 
 long randNumber;
-
+int isOff = HIGH;
 int Triggered = 3;
 //The pin that the our headphone jack is connected to.
 
@@ -16,37 +16,36 @@ void setup() {
  
 pinMode(RELAY1, OUTPUT);
 pinMode(Triggered, INPUT);
-
-
+//Serial.begin(9600);
 }
 
 void loop() {
 
  if (digitalRead(Triggered) == HIGH) {
-
  //RELAY1 is on for a minute and RELAY2 is off for a minute
- digitalWrite(RELAY1, LOW);
+ digitalWrite(RELAY1, !isOff);
  delay(60000); 
 
- digitalWrite(RELAY1, HIGH);               
+ digitalWrite(RELAY1, isOff);               
  delay(60000); 
- 
- do {
-  randNumber = random(30000 , 120000); 
+ do 
+ {
+    randNumber = random(30000 , 120000); 
   //randomizes between 30 seconds and 2 minutes
-  Serial.println(randNumber);
-  digitalWrite(RELAY1, LOW);
-  delay(randNumber);
+//  Serial.println(randNumber);
+    digitalWrite(RELAY1, !isOff);
+    delay(randNumber);
   
-  randNumber = random(30000 , 120000);
-  digitalWrite(RELAY1, HIGH); 
-  delay(randNumber);
-  counter--; //Count down from 3
+    randNumber = random(30000 , 120000);
+    digitalWrite(RELAY1, isOff); 
+    delay(randNumber);
+    counter--; //Count down from 3
  
- } while(counter > 0);
+  } while(counter > 0);
 
-}
-else {
-digitalWrite(RELAY1, HIGH);
-}
+  }
+  else 
+  {
+    digitalWrite(RELAY1, isOff);
+  }
 }
